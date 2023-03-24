5e21bcfd8e39840ca716a1fe7b4618b8aa43fc51
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 13:03:36 -0500

    added task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_leaves - counts the number of leaves in a tree
+ *
+ * @tree: root of tree
+ * Return: number of leaves
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	int leafCountLeft = 0;
+	int leafCountRight = 0;
+
+	if (tree == NULL)
+		return (0);
+	leafCountLeft = binary_tree_leaves(tree->left);
+	leafCountRight = binary_tree_leaves(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (leafCountLeft + leafCountRight);
+}
