6a050b635670d4166ee1f60865337a3826c1c0d1
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 12:56:04 -0500

    fixed 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,7 +14,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	for (count = 0; tree != NULL; tree = tree->parent, count++)
+	for (count = 0; tree->parent != NULL; tree = tree->parent, count++)
 	{}
 	return (count);
 }
