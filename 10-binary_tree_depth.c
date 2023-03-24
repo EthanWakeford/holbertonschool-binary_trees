0fcf2e8384669eab27be4337010e118689c1caa4
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 12:54:42 -0500

    added task 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - returns the depth of the node
+ *
+ * @tree: pointer to node
+ *Return: the depth
+*/
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t count;
+
+	if (tree == NULL)
+		return (0);
+
+	for (count = 0; tree != NULL; tree = tree->parent, count++)
+	{}
+	return (count);
+}
