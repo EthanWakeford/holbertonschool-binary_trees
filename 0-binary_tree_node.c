d34c453b7918f06bc615724701ecd4450b57bbcd
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 12:05:10 -0500

    added task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,10 +1,24 @@
 #include "binary_trees.h"
 
-
+/**
+ * binary_tree_node - adds a new node into a binary tree
+ *
+ * @parent: parent node to add child to
+ * @value: value of new node to add
+ * Return: pointer to new node
+*/
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node;
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	
+	if (node == NULL)
+		return (NULL);
 
-	tree imma tree
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+
+	return (node);
 }
