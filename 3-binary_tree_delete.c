5bccd99dcd2be86efda217a48ea0db77e4d7145e
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 12:32:15 -0500

    added task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes a binary tree
+ *
+ * @tree: pointer to root of tree
+ * Return: none
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+	free(tree);
+}
