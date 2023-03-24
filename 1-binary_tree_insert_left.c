54d48c04ae0e3228867bbd0a220ae18c6de7ac22
EthanWakeford <ethanwakeford@gmail.com>
2023-03-24 12:15:35 -0500

    added task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - adds a new node to the left
+ *
+ * @parent: parent of new node
+ * @value: value of new node
+ * Return: pointer to new node, or NULL on failure
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node = binary_tree_node(parent, value);
+
+	if (node == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+
+	if (parent->left == NULL)
+	{
+		parent->left = node;
+	}
+	else if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+		parent->left = node;
+	}
+
+	return (node);
+}
